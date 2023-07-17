0144a7dd27bf40a0025e00d9bffc6c8944aabac6
+ <+>
2023-07-17 16:04:13 -0400

    Comleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, message){
+    const messages = [];
+
+
+    for(i =0; i<names.length; i++){
+        messages.push("Thank you, ${names[i]}, for the wonderful ${message} gift!")
+    }
+    return messages;
+}
+console.log(writeCards(["Anzal", "Abdi", "Adan"],"surprise"))
+writeCards([i]);
+
+function countDown(number){
+    while(number>=0){
+        console.log(number);
+        number--;
+    }
+}
+countDown(20);
